b05f841f7a7df3cf065aa32019464f89687b6792
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-08 13:37:08 +0900

    [level 0] Title: 문자열 돌리기, Time: 17.51 ms, Memory: 7.32 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181945. 문자열 돌리기/문자열 돌리기.py
@@ -0,0 +1,12 @@
+str = input()
+
+for i in range(len(str)):
+    print(str[i])
+    i+=1
+
+#줄바꿈은 자동이고 줄바꿈 안원하면 print(strip.str[i], end='')
+
+
+
+#print('\n'.join(input()))
+# join은 줄바꿈을 사이사이 넣어줌 *을 넣으면 *을 사이사이 넣어줌
