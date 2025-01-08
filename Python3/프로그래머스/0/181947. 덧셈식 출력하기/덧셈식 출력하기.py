679488cc7b447a7b4dbdd9890189ab1c5224f6da
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-08 13:36:52 +0900

    [level 0] Title: 덧셈식 출력하기, Time: 17.41 ms, Memory: 7.52 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181947. 덧셈식 출력하기/덧셈식 출력하기.py
@@ -0,0 +1,7 @@
+a, b = map(int, input().strip().split(' '))
+
+print("%d + %d = %d"%(a,b,a+b))
+
+#a, b = map(int, input().strip().split(' '))
+#print(f"{a} + {b} = {a + b}")
+#f-string을 사용하여 변수와 연산 결과를 문자열에 포한하는 방법
