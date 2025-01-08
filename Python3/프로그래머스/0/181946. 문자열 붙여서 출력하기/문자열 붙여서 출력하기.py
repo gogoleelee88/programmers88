1ed7fcfcd6c09507ba35f44f7dc2dd01864da550
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-08 13:37:01 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 26.24 ms, Memory: 7.42 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.py
@@ -0,0 +1,9 @@
+#str1, str2 = input().strip().split(' ')
+
+#print(str1+str2)
+
+#print(input().strip().replace(' ', ''))
+#sprip 문자열 양쪽 공백, 줄바꿈 문자를 제거
+#replace(' ','')모든 공백을 제거
+
+print(input().replace(' ', ''))
