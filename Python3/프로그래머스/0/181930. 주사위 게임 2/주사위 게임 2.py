eb76af19d7d5be6d8b16bf3fb5f5977455775d09
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-13 10:51:28 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- Python3/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
+++ Python3/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -13,3 +13,22 @@ def solution(a, b, c):
         answer = (a + b + c) * (a**2 + b**2 + c**2 ) * (a**3 + b**3 + c**3 )
         
     return answer
+
+
+
+
+#def solution(a, b, c):
+#    check=len(set([a,b,c]))
+#    if check==1:
+#        return 3*a*3*(a**2)*3*(a**3)
+#    elif check==2:
+#        return (a+b+c)*(a**2+b**2+c**2)
+#    else:
+#        return (a+b+c)
+
+#def solution(a, b, c):
+#    list = [a, b, c]
+#    answer = 1
+#    for i in range(4-len(set(list))):
+#        answer *= a**(i+1)+b**(i+1)+c**(i+1)
+#    return answer
