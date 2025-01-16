c7c5216ce244394a635cbbeb1faeae1a2badc1f6
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-16 10:24:36 +0900

    [level 0] Title: 수열과 구간 쿼리 4, Time: 44.68 ms, Memory: 10.4 MB -BaekjoonHub

--- Python3/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
+++ Python3/프로그래머스/0/181922. 수열과 구간 쿼리 4/수열과 구간 쿼리 4.py
@@ -10,6 +10,18 @@ def solution(arr, queries):
             for [s,e,k] in queries:
                 if s<=i<=e and i%k==0:
                     arr[i]+=1
-    
+   # 여기  s<=i<=e 이조건을 쓰지 않았음
     # arr을 리턴한다.
         return arr
+    
+    
+    #def solution(arr, queries):
+    #for s, e, k in queries:
+    #    for i in range(s, e+1):
+    #        if i%k == 0:
+    #            arr[i] += 1
+    #return arr//
+    
+    
+    
+    
