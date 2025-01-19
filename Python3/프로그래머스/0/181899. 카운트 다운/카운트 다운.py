380c20732b5958112855faec31e91f41a417dc9d
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-01-19 17:50:41 +0900

    [level 0] Title: 카운트 다운, Time: 0.01 ms, Memory: 9.96 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/181899. 카운트 다운/카운트 다운.py
@@ -0,0 +1,10 @@
+def solution(start_num, end_num):
+    return sorted(range(end_num, start_num + 1), reverse=True)
+
+
+
+#def solution(start, end):
+#    return list(range(start,end-1,-1))
+
+#def solution(start, end):
+#    return [i for i in range(start,end-1,-1)]
