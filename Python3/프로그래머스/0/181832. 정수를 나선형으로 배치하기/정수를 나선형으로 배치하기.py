ef4a45a7e399e4fdc8dd7b0f43d0f84f140a62a2
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-02-04 13:52:12 +0900

    [level 0] Title: 정수를 나선형으로 배치하기, Time: 0.46 ms, Memory: 10.2 MB -BaekjoonHub

--- Python3/프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
+++ Python3/프로그래머스/0/181832. 정수를 나선형으로 배치하기/정수를 나선형으로 배치하기.py
@@ -22,4 +22,17 @@ def solution(n):
         
     
     
-    
+    #def solution(n):
+#    answer = [[None for j in range(n)] for i in range(n)]
+ #   move = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+ #   x, y, m = 0, 0, 0
+ #   for i in range(1, n**2 + 1):
+ #       answer[y][x] = i
+ #       if y + move[m][0] >= n or x + move[m][1] >= n or answer[y + move[m][0]][x + move[m][1]]:
+  #          m = (m + 1) % len(move)
+  #      y, x = y + move[m][0], x + move[m][1]
+  #  return answer
+
+#def solution(n):
+#    return [[ 4*i*(n-i) + (1+x+y - 2*i if (x==i or y==n-i-1) else (4*n - 6*i - x - y -3))  for i,x,y in row]   for row in [[(min(n-x-1,x,n-y-1,y),x,y) for y in range(n)] for x in range(n)]]
+
