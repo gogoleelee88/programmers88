0ab13430f9671e45b7fbd60c58ad0bc72c7f4837
gogoleelee88 <165794386+gogoleelee88@users.noreply.github.com>
2025-02-06 14:39:25 +0900

    [level 0] Title: 두 수의 곱, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120804. 두 수의 곱/두 수의 곱.py
@@ -0,0 +1,26 @@
+solution = lambda num1, num2: num1*num2
+
+
+#def solution(num1, num2):
+#   i=0
+#   answer=0
+#   for i<num1:
+#        answer+=num2
+#        i+=1
+#   return answer
+
+#def solution(num1, num2):
+#    answer = 0
+#    for i in range(num1):
+#        answer += num2
+#    return answer
+
+# 타입 힌트 있는 버전
+#def solution(num1: int, num2: int) -> int:
+#    answer = num1 * num2
+#    return answer
+
+# 타입 힌트 없는 버전
+#def solution(num1, num2):
+#    answer = num1 * num2
+#    return answer
